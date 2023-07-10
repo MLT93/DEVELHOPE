1c7fbdfb37522823d96ee643f2332f18d3c53825
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-07-10 13:58:16 +0200

    .

---
+++ CSS/9-MakeResponisveNav/start/main.js
@@ -0,0 +1,26 @@
+// ------ función que me permite navegar por el dropdown del NavBar 
+function nav(value) {
+  if (value !== "") {
+    location.href = value;
+    document.getElementById("dropdown").value = ""; // Reinicia el dropdown
+  }
+}
+
+// ------ funcion que permite acceder al buscador de Google
+
+document.addEventListener('DOMContentLoaded', function() {
+  let searchForm = document.getElementById("search-form");
+  let searchInput = document.getElementById("search-input");
+  
+  // Evita que el formulario se envíe
+  searchForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    
+    let searchTerm = searchInput.value;
+    let searchUrl = `https://www.google.com/search?q= ${encodeURIComponent(searchTerm)}`;
+    
+    // Redirige al usuario a la página de búsqueda de Google
+    window.location.href = searchUrl;
+    document.getElementById("search-input").value = "";
+  });
+});
