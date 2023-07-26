4bb49aa37bb33057c607c7618c051124599c8d9d
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-07-26 16:08:23 +0200

    update

--- JAVASCRIPT/2_MiddleLevel_JS/Exercise 52 - Array Methods/exercise.js
+++ JAVASCRIPT/2_MiddleLevel_JS/Exercise 52 - Array Methods/exercise.js
@@ -3,3 +3,7 @@ const students = [
   { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
   { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
 ];
+
+/* En el índice 2, elimino 1 elemento y agrego al array el nuevo objeto */
+students.splice(2, 1, students[students.length - 1] = {id: 3, name: "Francesco", surname: "Verdi", age: 41});
+console.log(students);
