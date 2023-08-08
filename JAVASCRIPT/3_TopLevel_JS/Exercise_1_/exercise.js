189d0a408a7143f33bfc7680c5747e40d931f979
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-08-08 20:31:34 +0200

    feat: new order

--- JAVASCRIPT/3_TopLevel_JS/Exercise_1_/exercise.js
+++ JAVASCRIPT/3_TopLevel_JS/Exercise_1_/exercise.js
@@ -1,11 +1,11 @@
 // Class definition
 
 class Person {
-    constructor(firstName, lastName) {
-        this.nombre = firstName;
-        this.apellido = lastName;
-    }
+  constructor(firstName, lastName) {
+    this.nombre = firstName;
+    this.apellido = lastName;
+  }
 }
 
-const developer = new Person('Mario', 'Rossi');
+const developer = new Person("Mario", "Rossi");
 console.log(developer.nombre + " " + developer.apellido);
