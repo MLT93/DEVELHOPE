4a3dc416a0e11fc2427981ab0b911c24d2a333a9
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-08-22 17:58:56 +0200

    feat: update

--- JAVASCRIPT/3_TopLevel_JS/Exercise_13_Rest parameters and spread syntax/exercise.js
+++ JAVASCRIPT/3_TopLevel_JS/Exercise_13_Rest parameters and spread syntax/exercise.js
@@ -3,4 +3,5 @@ function sum(num1, num2, num3) {
 }
 
 const numbers = [1, 2, 3];
+/* Pasando los elementos individuales del array numbers como argumentos a la función utilizando el Spread Operator */
 console.log(sum(...numbers));
