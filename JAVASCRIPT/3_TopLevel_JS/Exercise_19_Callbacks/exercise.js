17a0d954dda1ad17f67e501a651f3b7bbf9d8d8c
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-08-24 21:09:16 +0200

    update

--- JAVASCRIPT/3_TopLevel_JS/Exercise_19_Callbacks/exercise.js
+++ JAVASCRIPT/3_TopLevel_JS/Exercise_19_Callbacks/exercise.js
@@ -1,1 +1,14 @@
+const hello = "Hello";
 
+const variableString = "how are you?";
+
+let myInterval = setInterval(
+  (otherFunction = (variable) => {
+    console.log(variable);
+  }),
+  1000
+);
+
+const printAsyncName = (otherFunction, text) => {
+  setInterval;
+};
