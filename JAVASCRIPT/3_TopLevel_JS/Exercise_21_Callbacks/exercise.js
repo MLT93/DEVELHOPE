b8f645046c83a3f0c9ad8157d60c8e1199fba54a
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-08-25 16:46:07 +0200

    feat: update

--- JAVASCRIPT/3_TopLevel_JS/Exercise_21_Callbacks/exercise.js
+++ JAVASCRIPT/3_TopLevel_JS/Exercise_21_Callbacks/exercise.js
@@ -1,1 +1,17 @@
 
+const functionCallback = () => {
+  console.log("Hello");
+};
+
+const repeatHello = (otherFunction) => {
+  let intervalID = setInterval(() => {
+    otherFunction();
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(intervalID);
+    console.log("Repetición detenida");
+  }, 5000);
+};
+
+repeatHello(functionCallback);
