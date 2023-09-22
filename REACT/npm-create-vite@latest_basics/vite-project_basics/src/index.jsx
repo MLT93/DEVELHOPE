a95b8cfa668de9547803979c2a068b49905a8ea8
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-09-22 20:18:32 +0200

    feat: Exercise 9

--- REACT/npm-create-vite@latest_basics/vite-project_basics/src/index.jsx
+++ REACT/npm-create-vite@latest_basics/vite-project_basics/src/index.jsx
@@ -2,9 +2,4 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <div>
-    <App />
-  </div>,
-);
-
+createRoot(document.getElementById("root")).render(<App />);
