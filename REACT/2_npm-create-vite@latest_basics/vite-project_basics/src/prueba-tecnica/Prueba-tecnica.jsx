e54554a27b8ca6c0ca69f97a88f99ce1bb1caf64
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-10-11 22:58:35 +0200

    feat: prueba-tecnica

---
+++ REACT/2_npm-create-vite@latest_basics/vite-project_basics/src/prueba-tecnica/Prueba-tecnica.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+
+export const Prueba = () => {
+  const [state, setState] = useState("lorem ipusm");
+  const [imageState, setImageState] = useState("image cat");
+  const urlFact = `https://catfact.ninja/fact`;
+  const urlImage = `https://api.thecatapi.com/v1/images/search?limit=10&rand`;
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const response = await fetch(urlFact);
+
+        if (!response.ok) {
+          throw new Error(
+            `La respuesta no ha encontrado resultado: (response = ${response.ok})`,
+          );
+        }
+
+        console.log(response);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setState(data.fact);
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [urlFact]);
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const response = await fetch(urlImage);
+
+        if (!response.ok) {
+          throw new Error(
+            `La respuesta no ha encontrado resultado: (response = ${response.ok})`,
+          );
+        }
+
+        console.log(response);
+
+        const data = await response.json();
+
+        const imageUrl = data.map((url) => {
+          return url.url;
+        });
+
+        // .splice(start, delete, add)
+        imageUrl.splice(0, 9);
+
+        console.log(imageUrl);
+
+        setImageState(imageUrl);
+      })();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [urlFact, urlImage]);
+
+  return (
+    <main>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          placeItems: "center",
+          width: "290px",
+          height: "auto",
+          border: "1.5px solid black",
+          borderRadius: "10px",
+          padding: "10px",
+          backgroundColor: 'lightcyan',
+        }}>
+        <img
+          src={imageState}
+          alt={`Image given of ${urlImage}`}
+          style={{
+            width: "98.5%",
+            height: "250px",
+            border: "1px solid black",
+            borderRadius: "10px",
+          }}
+        />
+        <div>
+          <h4>Information:</h4>
+          <p>{state && state}</p>
+        </div>
+      </div>
+    </main>
+  );
+};
