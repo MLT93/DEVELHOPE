464d206cfe83ac3e8c17ecd092e13e9b2edf0810
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-10-24 14:31:23 +0200

    feat: Exercise 83

--- REACT/2_npm-create-vite@latest_basics/vite-project_basics/src/App.jsx
+++ REACT/2_npm-create-vite@latest_basics/vite-project_basics/src/App.jsx
@@ -1,10 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import { Welcome } from './Exercises';
+import { Routes, Route } from "react-router-dom";
+import { Counter, Exercises, Welcome } from "./Exercises";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path='/:username' element={<Welcome />} />
+      <Route path="/" element={<Exercises />} />
+      <Route path="/:username" element={<Welcome />} />
+      <Route
+        path="/counter"
+        element={
+          <Counter initialValue={200} incrementAmount={4} decrementAmount={2} />
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
