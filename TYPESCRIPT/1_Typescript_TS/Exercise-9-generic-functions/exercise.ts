8a7f58f2f37b342b4586f3765362d3b470d81817
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-10-29 23:05:31 +0100

    feat: Exercise 9

--- TYPESCRIPT/1_Typescript_TS/Exercise-9-generic-functions/exercise.ts
+++ TYPESCRIPT/1_Typescript_TS/Exercise-9-generic-functions/exercise.ts
@@ -6,8 +6,8 @@
 // This should fix the type errors on Line 13 and 15.
 // Hint: The type for the `value` function parameter should use a type variable.
 
-function identity(value) {
-    return value;
+function identity<Type>(value: Type) {
+  return value;
 }
 
 let value1 = identity<string>("France");
@@ -16,23 +16,24 @@ let value2 = identity<number>(67_413_000);
 
 // ----
 
-async function fetchData<Type>(url: string) {
-    const response = await fetch(url);
-    const responseBody = await response.json();
+async function fetchData<Type>(url: string): Promise<Type> {
+  const response = await fetch(url);
+  const responseBody = await response.json();
 
-    return responseBody as Type;
+  return responseBody as Type;
 }
 
 interface User {
-    name: string;
+  name: string;
 }
 
 // Pass the `User` type as a type argument to the generic `fetchData()` function.
 // This should fix the type error on Line 35.
 
-let user = await fetchData("http://api.com/user/1");
-
-console.log(user.name);
+(async (fetchData) => {
+  let user = await fetchData<User>("http://api.com/user/1");
+  console.log(user.name);
+})(fetchData);
 
 // ----
 
