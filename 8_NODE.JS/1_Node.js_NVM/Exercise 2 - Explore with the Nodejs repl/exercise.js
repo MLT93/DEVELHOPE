60dc60d80b6a6f787296edee2275472e17b75e5b
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-11-02 12:14:55 +0100

    feat: Exercise 2

---
+++ 8_NODE.JS/1_Node.js_NVM/Exercise 2 - Explore with the Nodejs repl/exercise.js
@@ -0,0 +1,11 @@
+const crypto = require("node:crypto");
+
+/* Exercise 2: Explore with the Node.js REPL
+
+Use the Node.js REPL to list the methods provided by the Node.js core crypto module. Use one of these methods to generate a random ID. */
+
+const randomID = crypto.randomUUID();
+
+console.log(randomID);
+
+       
